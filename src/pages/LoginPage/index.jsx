d881b32a4bc19a3c0d8a2082d7c7357206b2d9ae
Luiz <luizsaulo@yahoo.com.br>
2022-08-02 21:47:19 -0300

    continuing with the login page

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,26 +1,43 @@
-import React from 'react'
-import './styles.css'
+import React, { useState } from "react";
+import "./styles.css";
 
 export default function LoginPage() {
-  const handleSubmit = () => {
-    console.log('submit');
-  }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("submit", {email, password});
+  };
   return (
-    <div id='login'>
-      <h1 className='title'>Login do sistema</h1>
-      <form className='form' onSubmit={handleSubmit}>
-        <div className='field'>
-          <label htmlFor='email'>Email</label>
-          <input type='email' name='email' id='email' />
+    <div id="login">
+      <h1 className="title">Login do sistema</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="field">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-        <div className='field'>
-          <label htmlFor='password'>Senha</label>
-          <input type='password' name='password' id='password' />
+        <div className="field">
+          <label htmlFor="password">Senha</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <div className='actions'>
-          <button type='submit'>Entrar</button>
+        <div className="actions">
+          <button type="submit">Entrar</button>
         </div>
       </form>
     </div>
-  )
+  );
 }
